fa7ca9aff7efb7ea95eca62fc49e295d08361016
Karan Sharma <karan1276@gmail.com>
2015-11-01 16:45:20 +0530

    Initial code copied and pasted from bs4 github respository

---
+++ gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				files: {
+					'css/main.css' : 'scss/bootstrap-grid.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	//grunt.registerTask('default',['sass','watch']);
+    grunt.registerTask('default',['sass']);
+}   
